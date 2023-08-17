dc99ee5f9632ebe6a8b133b22bd3d5a4c8356ca8
Leonardo Ocampo <leonardoj.ocampo@gmail.com>
2023-08-16 22:46:56 -0300

    agrega rama alternativo que contiene el tp2 funcionando

--- TP3/mainwindow.cpp
+++ TP3/mainwindow.cpp
@@ -1,49 +1,204 @@
+/****************************************************************************
+**
+** Copyright (C) 2012 Denis Shienkov <denis.shienkov@gmail.com>
+** Copyright (C) 2012 Laszlo Papp <lpapp@kde.org>
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the QtSerialPort module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:BSD$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** BSD License Usage
+** Alternatively, you may use this file under the terms of the BSD license
+** as follows:
+**
+** "Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are
+** met:
+**   * Redistributions of source code must retain the above copyright
+**     notice, this list of conditions and the following disclaimer.
+**   * Redistributions in binary form must reproduce the above copyright
+**     notice, this list of conditions and the following disclaimer in
+**     the documentation and/or other materials provided with the
+**     distribution.
+**   * Neither the name of The Qt Company Ltd nor the names of its
+**     contributors may be used to endorse or promote products derived
+**     from this software without specific prior written permission.
+**
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <string>
-#include <QDebug>
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
-    serial= new QSerialPort();
-    serial->setPortName("COM9");
-    serial->open(QSerialPort::ReadOnly);
-    serial->setBaudRate(QSerialPort::Baud9600);
-    serial->setDataBits(QSerialPort::Data8);
-    serial->setFlowControl(QSerialPort::NoFlowControl);
-    serial->setParity(QSerialPort::NoParity);
-    serial->setStopBits(QSerialPort::OneStop);
-    serial->open(QIODevice::ReadWrite);
-    connect(serial, SIGNAL(readyRead()), this, SLOT(RecibirArreglo()));
+#include "console.h"
+#include "settingsdialog.h"
 
-    if(serial->isOpen())
-    {
-        qDebug() <<"serial is connected.";
-    }
-    else
-    {
-       qDebug () <<"serial in not connected.";
-    }
-}
-void MainWindow::RecibirArreglo()
+#include <QLabel>
+#include <QMessageBox>
+#include <QFile>
+#include <QByteArray>
+#include <QDebug>
+
+
+//! [0]
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    m_ui(new Ui::MainWindow),
+    m_status(new QLabel),
+    m_settings(new SettingsDialog),
+//! [1]
+    m_serial(new QSerialPort(this))
+//! [1]
 {
-    QByteArray arreglo;
-    arreglo=serial->readLine();
-     cadena=cadena+arreglo;
-    /*if(cadena.length()<4){*/
-if(cadena.contains("9")){
-        ui->plainTextEdit->appendPlainText(cadena);
-        cadena="";
+//! [0]
+    m_ui->setupUi(this);
+    setCentralWidget(m_console);
+
+    m_ui->actionConnect->setEnabled(true);
+    m_ui->actionDisconnect->setEnabled(false);
+    m_ui->actionQuit->setEnabled(true);
+    m_ui->actionConfigure->setEnabled(true);
+
+    m_ui->statusBar->addWidget(m_status);
+
+    initActionsConnections();
+
+    connect(m_serial, &QSerialPort::errorOccurred, this, &MainWindow::handleError);
+
+//! [2]
+    connect(m_serial, &QSerialPort::readyRead, this, &MainWindow::readData);
+//! [2]
+//! [3]
 }
-}
+//! [3]
+
 MainWindow::~MainWindow()
 {
-    delete ui;
-    serial->close();
+    delete m_settings;
+    delete m_ui;
+}
+
+//! [4]
+void MainWindow::saveData(const QByteArray &data)
+{
+    QFile file("archivo.bin");
+    if(file.open(QIODevice::Append)){
+        qint64 bytesWritten = file.write(data);
+        if(bytesWritten == -1)
+            qDebug() << "Error al escribir el archivo";
+        else{
+            qDebug() << "Se escribieron" << bytesWritten << "bytes en el archivo";
+
+        }
+        file.close();
+    }
+
 }
 
 
+
+void MainWindow::openSerialPort()
+{
+    const SettingsDialog::Settings p = m_settings->settings();
+    m_serial->setPortName(p.name);
+    m_serial->setBaudRate(p.baudRate);
+    m_serial->setDataBits(p.dataBits);
+    m_serial->setParity(p.parity);
+    m_serial->setStopBits(p.stopBits);
+    m_serial->setFlowControl(p.flowControl);
+    if (m_serial->open(QIODevice::ReadWrite)) {
+        m_ui->actionConnect->setEnabled(false);
+        m_ui->actionDisconnect->setEnabled(true);
+        m_ui->actionConfigure->setEnabled(false);
+        showStatusMessage(tr("Conectado a %1 : %2, %3, %4, %5, %6")
+                          .arg(p.name).arg(p.stringBaudRate).arg(p.stringDataBits)
+                          .arg(p.stringParity).arg(p.stringStopBits).arg(p.stringFlowControl));
+    } else {
+        QMessageBox::critical(this, tr("Error"), m_serial->errorString());
+
+        showStatusMessage(tr("Error al abrir"));
+    }
+}
+//! [4]
+
+//! [5]
+void MainWindow::closeSerialPort()
+{
+    if (m_serial->isOpen())
+        m_serial->close();
+    m_ui->actionConnect->setEnabled(true);
+    m_ui->actionDisconnect->setEnabled(false);
+    m_ui->actionConfigure->setEnabled(true);
+    showStatusMessage(tr("Desconectado"));
+}
+//! [5]
+
+void MainWindow::about()
+{
+    QMessageBox::about(this, tr("Acerca de..."),
+                       tr("TP N2 para técnicas digitales II "
+                          "2023 UTN-FRC - Córdoba"));
+}
+
+//! [6]
+void MainWindow::writeData(const QByteArray &data)
+{
+    m_serial->write(data);
+}
+//! [6]
+
+//! [7]
+void MainWindow::readData()
+{
+    dataArchivo = m_serial->readAll();
+    saveData(dataArchivo);
+}
+//! [7]
+
+//! [8]
+void MainWindow::handleError(QSerialPort::SerialPortError error)
+{
+    if (error == QSerialPort::ResourceError) {
+        QMessageBox::critical(this, tr("Error crítico"), m_serial->errorString());
+        closeSerialPort();
+    }
+}
+//! [8]
+
+void MainWindow::initActionsConnections()
+{
+    connect(m_ui->actionConnect, &QAction::triggered, this, &MainWindow::openSerialPort);
+    connect(m_ui->actionDisconnect, &QAction::triggered, this, &MainWindow::closeSerialPort);
+    connect(m_ui->actionQuit, &QAction::triggered, this, &MainWindow::close);
+    connect(m_ui->actionConfigure, &QAction::triggered, m_settings, &SettingsDialog::show);
+    connect(m_ui->actionAbout, &QAction::triggered, this, &MainWindow::about);
+    connect(m_ui->actionAboutQt, &QAction::triggered, qApp, &QApplication::aboutQt);
+}
+
+void MainWindow::showStatusMessage(const QString &message)
+{
+    m_status->setText(message);
+}
 
 
